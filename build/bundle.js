233abcbae62d1bdf4f1e89d7bf90f8f40f790a5b
archieDeveloper <arkadij.ok@gmail.com>
2015-10-28 22:36:27 +0700

    Переписал главной файл так чтобы он работал :)

--- build/bundle.js
+++ build/bundle.js
@@ -44,211 +44,125 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var Vector2d, a, b;
+	var CMRequestAnimationFrame, canvas, context, draw, gameLoop, init, lastTime, loadImage, resource, step;
 
-	Vector2d = __webpack_require__(1);
+	resource = __webpack_require__(2);
 
-	a = Vector2d(1, 3);
+	CMRequestAnimationFrame = (function() {
+	  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+	    window.setTimeout(callback, 1000 / 60);
+	  };
+	})();
 
-	b = Vector2d(2, 1);
+	canvas = document.createElement("canvas");
 
-	a.add(b);
+	context = canvas.getContext("2d");
 
-	console.log(a);
+	lastTime = Date.now();
 
+	step = function() {};
 
-	/*
-	window.object = {}
-	window.sprite = {}
+	draw = function() {
+	  return context.clearRect(0, 0, canvas.width, canvas.height);
+	};
 
-	requestAnimFrame = do ->
-	    return window.requestAnimationFrame    ||
-	        window.webkitRequestAnimationFrame ||
-	        window.mozRequestAnimationFrame    ||
-	        window.oRequestAnimationFrame      ||
-	        window.msRequestAnimationFrame     ||
-	        (callback)->
-	            window.setTimeout callback, 1000/60
+	gameLoop = function() {
+	  var dt, fps, now;
+	  now = Date.now();
+	  dt = (now - lastTime) / 1000.0;
+	  fps = 1000.0 / (now - lastTime);
+	  step();
+	  draw();
+	  context.fillStyle = "#000";
+	  context.font = "12pt Arial";
+	  context.fillText('fps: ' + Math.round(fps), 20, 20);
+	  lastTime = now;
+	  return CMRequestAnimationFrame(gameLoop);
+	};
 
-	canvas = document.createElement "canvas"
-	window.canvas = canvas
+	init = function() {
+	  canvas.width = 800;
+	  canvas.height = 600;
+	  document.body.appendChild(canvas);
+	  return gameLoop();
+	};
 
-	loadImage = ->
-	    arrayUrlImg = []
-	    for (var item in sprites) {
-	        if(typeof sprites[item].img === 'string'){
-	            arrayUrlImg.push('./coffeeMaker/app/images/'+sprites[item].img);
-	        }
-	    }
-	    for (var first_room in rooms) {
-	        if(typeof rooms[first_room].background === 'string'){
-	            arrayUrlImg.push('./coffeeMaker/app/images/'+rooms[first_room].background);
-	        }
-	    }
-	    resources.load arrayUrlImg
-	    resources.onReady init
+	loadImage = function() {
+	  return init();
+	};
 
-	initSprites = ->
-	    for (var item in sprites) {
-	        if (typeof sprites[item].img === "string") {
-	            sprites[item].img = resources.get('./coffeeMaker/app/images/'+sprites[item].img);
-	        }
-	        if (typeof sprites[item].originX === "string" && sprites[item].originX === 'center') {
-	            sprites[item].originX = sprites[item].width/2;
-	        }
-	        if (typeof sprites[item].originY === "string" && sprites[item].originY === 'center') {
-	            sprites[item].originY = sprites[item].height/2;
-	        }
-	        sprite[item] = new CMSprite(sprites[item]);
-	    }
-
-	    for (var first_room in rooms) {
-	        if (typeof rooms[first_room].background === "string") {
-	            rooms[first_room].background = resources.get('./coffeeMaker/app/images/'+rooms[first_room].background);
-	        }
-	        room = new CMRoom(rooms[first_room]);
-	        window.room = room;
-	        break;
-	    }
-
-	    for (var item in sprites) {
-	        if (typeof sprites[item].img === "string") {
-	            sprites[item].img = resources.get('./coffeeMaker/app/images/'+sprites[item].img);
-	        }
-	        if (typeof sprites[item].originX === "string" && sprites[item].originX === 'center') {
-	            sprites[item].originX = sprites[item].width/2;
-	        }
-	        if (typeof sprites[item].originY === "string" && sprites[item].originY === 'center') {
-	            sprites[item].originY = sprites[item].height/2;
-	        }
-	        sprite[item] = new CMSprite(sprites[item]);
-	    }
-
-	init = ->
-	    do initSprites
-	    ctx = canvas.getContext "2d"
-	    canvas.width = setting.windowWidth
-	    canvas.height = setting.windowHeight
-	    document.body.appendChild canvas
-	    window.ctx = ctx
-	    for (var i = 0, roomObjectsLen = room.objects.length - 1; i <= roomObjectsLen; i++) {
-	        instanceCreate(room.objects[i][0],room.objects[i][1],room.objects[i][2]);
-	    }
-	    lastTime = do Date.now
-	    do gameLoop
-
-	gameLoop = ->
-	    now = do Date.now
-	    dt = (now - lastTime) / 1000.0
-	    fps = 1000.0 /(now - lastTime)
-
-	    do step
-	    do clearEvent
-	    do draw
-
-	    ctx.fillStyle = "#000"
-	    ctx.font = "12pt Arial"
-	    ctx.fillText 'fps: '+Math.round(fps), 20, 20
-
-	    lastTime = now
-	    requestAnimFrame gameLoop
-
-	step = ->
-	    for (var collection in object) {
-	        for (var item in object[collection].id) {
-	            object[collection].id[item].step();
-	        }
-	    }
-	    for (var i = 0, roomViewLen = room.view.length; i < roomViewLen; i++) {
-	        if (room.view[i].x <= 0) { room.view[i].x = 0; }
-	        if (room.view[i].y <= 0) { room.view[i].y = 0; }
-	        if ((room.view[i].x+room.view[i].width) >= room.width) {
-	            room.view[i].x = room.width-room.view[i].width;
-	        }
-	        if ((room.view[i].y+room.view[i].height) >= room.height) {
-	            room.view[i].y = room.height-room.view[i].height;
-	        }
-	    }
-
-	draw = ->
-	    ctx.clearRect 0, 0, canvas.width, canvas.height
-
-	    for (var i = 0, roomViewLen = room.view.length; i < roomViewLen; i++) {
-
-	        if (room.background != null) {
-	            ws = Math.floor(room.view[i].x/room.background.width);
-	            wf = Math.ceil((room.view[i].x+room.view[i].width)/room.background.width);
-
-	            hs = Math.floor(room.view[i].y/room.background.height);
-	            hf = Math.ceil((room.view[i].y+room.view[i].height)/room.background.height);
-
-	            for (var h = hs; h < hf; h++) {
-	                for (var w = ws; w < wf; w++) {
-
-	                    var x = room.background.width*w,
-	                        y = room.background.height*h;
-
-	                    ctx.save();
-
-	                    ctx.beginPath();
-	                    ctx.rect(
-	                        room.view[i].offset.x,
-	                        room.view[i].offset.y,
-	                        room.view[i].width,
-	                        room.view[i].height
-	                        );
-	                    ctx.clip();
-
-	                    ctx.drawImage(
-	                        room.background,
-	                        x-room.view[i].x+room.view[i].offset.x,
-	                        y-room.view[i].y+room.view[i].offset.y,
-	                        room.background.width,
-	                        room.background.height
-	                        );
-
-	                    ctx.restore();
-	                }
-	            }
-	        }
-
-	        for (var collection in object) {
-	            for (var item in object[collection].id) {
-	                object[collection].id[item].draw();
-	            }
-	        }
-
-	        setMyCursor s.cursor
-	    }
-
-	window.onload = loadImage
-	 */
+	window.onload = loadImage;
 
 
 /***/ },
-/* 1 */
+/* 1 */,
+/* 2 */
 /***/ function(module, exports) {
 
-	var Vector2d;
+	var Resource, _load, loading, readyCallbacks, resourceCache;
 
-	Vector2d = (function() {
-	  function Vector2d(x, y) {
-	    this.x = x;
-	    this.y = y;
+	resourceCache = {};
+
+	loading = [];
+
+	readyCallbacks = [];
+
+	_load = function(url) {
+	  var img;
+	  if (resourceCache[url]) {
+	    return resourceCache[url];
+	  } else {
+	    img = new Image;
+	    img.onload = function() {
+	      resourceCache[url] = img;
+	      if (isReady()) {
+	        return readyCallbacks.forEach(function(func) {
+	          return func();
+	        });
+	      }
+	    };
+	    resourceCache[url] = false;
+	    return img.src = url;
 	  }
+	};
 
-	  Vector2d.prototype.add = function(b) {
-	    var cx, cy;
-	    cx = this.x + b.x;
-	    cy = this.y + b.y;
-	    return new this(cx, cy);
+	Resource = (function() {
+	  function Resource() {}
+
+	  Resource.prototype.load = function(urlOrArr) {
+	    if (urlOrArr instanceof Array) {
+	      return urlOrArr.forEach(function(url) {
+	        return _load(url);
+	      });
+	    } else {
+	      return _load(urlOrArr);
+	    }
 	  };
 
-	  return Vector2d;
+	  Resource.prototype.get = function(url) {
+	    return resourceCache[url];
+	  };
+
+	  Resource.prototype.isReady = function() {
+	    var k, ready;
+	    ready = true;
+	    for (k in resourceCache) {
+	      if (resourceCache.hasOwnProperty(k) && !resourceCache[k]) {
+	        ready = false;
+	      }
+	    }
+	    return ready;
+	  };
+
+	  Resource.prototype.onReady = function(func) {
+	    return readyCallbacks.push(func);
+	  };
+
+	  return Resource;
 
 	})();
 
-	module.exports = Vector2d;
+	module.exports = Resource;
 
 
 /***/ }
--- source/core/engine/class/vector2d.coffee
+++ source/core/engine/class/vector2d.coffee
@@ -1,9 +1,55 @@
 class Vector2d
     constructor: (@x, @y)->
 
+    # сложение векторов
     add: (b)->
-        cx = @x + b.x
-        cy = @y + b.y
-        new @(cx,cy);
+        @x += b.x
+        @y += b.y
+        @
+
+    # вычитание векторов
+    sub = (b)->
+        @x -= b.x
+        @y -= b.y
+        @
+
+    # умножение вектора на скаляр
+    mul = (scalar)->
+        @x * scalar
+        @y * scalar
+        @
+
+    # деление вектора на скаляр
+    div = (scalar)->
+        @x /= scalar
+        @y /= scalar
+        @
+
+    # умножение векторов
+    mulScalar = (b)->
+        @x *= b.x
+        @y *= b.y
+        @
+
+    # длина вектора
+    length = ()->
+        Math.sqrt @x*@x+@y*@y
+
+    # нормализация вектора
+    normalize = ()->
+        len = do @length
+        @x /= len
+        @y /= len
+        @
+
+    # проецирование вектора
+    projection = (b)->
+        c = do b.normalize
+        scl = @mulScalar(b)
+        c.mul(scl)
+
+    # поворот вектора
+    rotate = (a)->
+        Math.atan2(a.y,a.x)*180/Math.PI
 
 module.exports = Vector2d
